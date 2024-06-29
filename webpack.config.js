1cee240ee1af5e67f91e8c2a7eb5a61efb8c7e8e
samuelauto <samuelme@automatica.edu.cujae.cu>
2024-06-29 19:10:35 -0400

    Configuraciones iniciales listas

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+        },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename:'index.html',
+      inject: 'head',
+      scriptLoading: 'defer'
+      }),
+  ]
+};
